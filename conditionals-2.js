a4ad5c74d1f5aab11ab1cc02c0007050a1c418d0
Michael Seth <michael.a.seth175@gmail.com>
2021-02-03 15:14:45 -0500

    added structure for chimichanga game

---
+++ conditionals-2.js
@@ -0,0 +1,8 @@
+// 1. Get the user to enter a Number
+// 2. store that number in a var
+// 3. use conditional statement to check if the number is divisible by 3
+//     If it is alert chimis
+// 4. check if number is divisble by 5
+//     if it is alert changas
+// 5. check if num is divisible by 3 & 5
+//     alert chimichangas
