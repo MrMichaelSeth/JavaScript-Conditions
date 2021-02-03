4dd910e509239078ac1ff268446e05266d38d358
Michael Seth <michael.a.seth175@gmail.com>
2021-02-03 15:01:41 -0500

    make random number guessing game

--- conditionals-1.js
+++ conditionals-1.js
@@ -1,1 +1,17 @@
-console.log("connected");
+/* make the random number*/
+/* store it the random number in value*/
+
+let randomNum = Math.floor((Math.random() * 3) + 1);
+
+/* prompt the user to guess a number*/
+/* store that number in a var*/
+
+let userNum = prompt("Guess a number between 1 and 3");
+
+/* create a conditional statement to check the random number against the guessed number */
+
+if ( userNum == randomNum) {
+    alert("You guessed correctly! Congrats!")
+} else {
+    alert(`You guessed incorrectly, the number was: ${randomNum}. Reload the page and try again!`)
+}
